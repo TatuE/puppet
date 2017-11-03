b557ec925e7a9016c99acf790ee7b18e26c77a58
TatuE <tatu.erkinjuntti@gmail.com>
2017-11-03 21:14:45 +0200

    Updated mysql module -> password

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -1,17 +1,27 @@
 class mysql {
+	$mysqladmin = '/usr/bin/mysqladmin'
 
 	package {'mysql-server':
 		ensure => installed,
 	}
+
 	package {'mysql-client':
 		ensure => installed,
 		require => Package ['mysql-server'],
 	}
+
 	package {'php-mysql':
 		ensure => installed,
 		require => Package ['mysql-client'],
 		notify => Service ['mysql'],
 	}
+
+	exec {'mysqlpasswd':
+		command => "${mysqladmin} -u root password salasana",
+		require => Package ['mysql-server'],
+		notify => Service ['mysql'],
+	}
+
 	service {'mysql':
 		ensure => running,
 		enable => true,
